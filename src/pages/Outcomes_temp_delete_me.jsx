40bd77fa6c26add5d53534667c5bd364367e4e8f
graham.currie <graham.currie@candorasociety.com>
2026-06-05 07:23:22 +0000

    File changes

--- src/pages/Outcomes_temp_delete_me.jsx
+++
@@ -1,6 +0,0 @@
-// This is a stub file to delete the Outcomes page
-// The Outcomes functionality has been moved to the Reports page
-// This file should be deleted
-export default function Outcomes() {
-  return null;
-}
